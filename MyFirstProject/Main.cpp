44a9a90e852f8a3334e0de5bc9d6e474d1f73f8d
KyleNap07 <kylenap937@gmail.com>
2025-09-04 10:38:55 -0500

    Update Main.cpp

--- MyFirstProject/Main.cpp
+++ MyFirstProject/Main.cpp
@@ -3,9 +3,12 @@ Kyle Napier
 C++ 1st Semester 2025
 9/9/2025
 Lab 1 MyFirstProject
-
+Succesfully output "Hello World!" and your favortite color
 */
 #include <iostream> 
 int main()
-std::cout << "Hello World!";
-return 0;
+{
+	std::cout << "\033[32;44mHello World!\033[0m";
+	std::cout << "\033[30;41m My Favorite Color Is Red!\033[0m";
+	return 0;
+}
